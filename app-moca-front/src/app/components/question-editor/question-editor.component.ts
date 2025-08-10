ff51dcea5964aa691603243ebac0b743fe34a8e9
esteban2502 <juanes9001@gmail.com>
2025-08-09 21:49:36 -0500

    agrega modales

--- app-moca-front/src/app/components/question-editor/question-editor.component.ts
+++ app-moca-front/src/app/components/question-editor/question-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TabsComponent } from '../tabs/tabs.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-question-editor',
@@ -9,5 +10,26 @@ import { TabsComponent } from '../tabs/tabs.component';
   styleUrl: './question-editor.component.css'
 })
 export class QuestionEditorComponent {
+
+  constructor(private modal: NgbModal){};
+
+   closeResult: string = '';
+
+
+    open(content: any) {
+    
+
+    this.modal
+      .open(content, { size: 'md', ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with:${result} `;
+        
+        },
+        (reason = close) => {
+          this.closeResult = 'Dismissed';
+        }
+      );
+  }
 
 }
--- app-moca-front/src/app/components/test-editor/test-editor.component.ts
+++ app-moca-front/src/app/components/test-editor/test-editor.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { TabsComponent } from '../tabs/tabs.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-test-editor',
   imports: [TabsComponent],
-  standalone:true,
+  standalone: true,
   templateUrl: './test-editor.component.html',
-  styleUrl: './test-editor.component.css'
+  styleUrl: './test-editor.component.css',
 })
 export class TestEditorComponent {
+  constructor(private modal: NgbModal) {}
+  closeResult: string = '';
 
+  open(content: any) {
+    this.modal
+      .open(content, { size: 'md', ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with:${result} `;
+        },
+        (reason = close) => {
+          this.closeResult = 'Dismissed';
+        }
+      );
+  }
 }
