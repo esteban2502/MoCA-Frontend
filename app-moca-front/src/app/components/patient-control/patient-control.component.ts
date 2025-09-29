ce2c8b8a1d90a1e143c48c035aa358e686d6bfcd
esteban2502 <juanes9001@gmail.com>
2025-09-28 23:09:52 -0500

    ajusta la maquetacion de control de pacientes

--- app-moca-front/src/app/components/patient-control/patient-control.component.ts
+++ app-moca-front/src/app/components/patient-control/patient-control.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TabsComponent } from '../tabs/tabs.component';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+import { UserEntityService } from '../../services/user-entity.service';
+import { UserEntity } from '../../models/UserEntity';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-patient-control',
-  imports: [TabsComponent, NgbTooltipModule],
-  standalone:true,
+  imports: [TabsComponent, NgbTooltipModule, CommonModule, NgxPaginationModule],
+  standalone: true,
   templateUrl: './patient-control.component.html',
   styleUrl: './patient-control.component.css'
 })
-export class PatientControlComponent {
+export class PatientControlComponent implements OnInit {
+  patients: UserEntity[] = [];
+  isLoading = false;
+  errorMessage = '';
 
+  itemsPerPage: number = 5;
+  p: number = 1;
+
+  constructor(private userEntityService: UserEntityService) {}
+
+  ngOnInit(): void {
+    this.loadPatients();
+  }
+
+  loadPatients(): void {
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.userEntityService.getAll().subscribe({
+      next: (data) => {
+        this.patients = data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error cargando pacientes:', error);
+        this.errorMessage = 'Error al cargar los pacientes';
+        this.isLoading = false;
+      }
+    });
+  }
 }
